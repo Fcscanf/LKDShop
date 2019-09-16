97c5d4dd7b7dbc4122d40194438772ccb7674381
Fcscanf <fcscanf@gmail.com>
2019-09-16 09:58:35 +0800

    [Codes-Fcant-Updates]:Init Pro

---
+++ src/main/java/com/fcant/shop/LkdShopApplication.java
@@ -0,0 +1,13 @@
+package com.fcant.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkdShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkdShopApplication.class, args);
+	}
+
+}
